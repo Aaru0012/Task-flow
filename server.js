fc90f20e9d3a0ed1e0ce6870e72584fef627ec01
Aaru0012 <aaru0012@users.noreply.github.com>
2026-05-20 14:23:42 +0530

    Initial project setup

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
